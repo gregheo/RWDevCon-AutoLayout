8a209d10d9874518cd029bffe58305b2ddd78e23
Greg Heo <greg@gregheo.com>
2014-12-02 11:46:35 -0500

    Wrap in invisible nav controller

--- 1-Starter/About RWDevCon/About RWDevCon/ViewController.swift
+++ 1-Starter/About RWDevCon/About RWDevCon/ViewController.swift
@@ -29,6 +29,17 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
   }
 
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+
+    navigationController?.setNavigationBarHidden(true, animated: animated)
+  }
+
+  override func viewWillDisappear(animated: Bool) {
+    super.viewWillDisappear(animated)
+    navigationController?.setNavigationBarHidden(false, animated: animated)
+  }
+
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
